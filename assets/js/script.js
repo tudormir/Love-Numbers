7cd1da7bf405618867c87def7d909bf2bfb29d93
tudormir <tudormir@hotmail.com>
2022-12-05 18:10:25 +0000

    The game is working for even number.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,23 @@ function displayNumber() {
     document.getElementById('operand').textContent=num;
 }
 displayNumber();
-function incrementScore() {
 
-}
 
-function incrementIncorrectAnswer() {
-
-}
-
-// When one of the buttons is pressed, than the DOM must check of the number correspond to the correct button.
+// When one of the buttons is pressed, than the DOM must check of the button correspond to the correct number.
 document.addEventListener("DOMContentLoaded", function() {
 	let buttons = document.getElementsByTagName("button");
 
 	for (let button of buttons) {
 		button.addEventListener("click", function() {
 			if (this.getAttribute("data-type") === "even") {
-				runGame("even")
+				
+                //alert("You've clicked Even button");
+                runGame("even");
 			} else if (this.getAttribute("data-type") === "odd") {
+                //alert("You've clicked odd button");
                 runGame("odd");
             } else if (this.getAttribute("data-type") === "prime") {
+                //alert("You've clicked Prime button");
                 runGame("prime");
             } 
 		});
@@ -32,9 +30,10 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 
     function runGame(gameType) {
-        
+        let num=parseInt(document.getElementById("operand").textContent);
         if (gameType === "even") {
             checkEvenAnswer(num);
+            displayNumber();
         } else if (gameType === "odd") {
             checkOddAnswer(num);
         } else if (gameType === "prime") {
@@ -53,3 +52,21 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
     }
+    function incrementScore() {
+
+        // Gets the current score from the DOM and increments it
+    
+        let oldScore = parseInt(document.getElementById("score").innerText);
+        document.getElementById("score").innerText = ++oldScore;
+    
+    }
+    
+    function incrementIncorrectAnswer() {
+    
+        // Gets the current tally of incorrect answers from the DOM and increments it
+    
+        let oldScore = parseInt(document.getElementById("incorrect").innerText);
+        document.getElementById("incorrect").innerText = ++oldScore;
+    
+    }
+    
